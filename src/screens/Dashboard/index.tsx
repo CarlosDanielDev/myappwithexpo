99263730125e46913279f6ff6f80557271dc8f08
Carlos Daniel <killer.dk20@gmail.com>
2021-07-16 00:05:09 -0300

    chore: organize files with styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+import * as S from './styles';
+
+export const Dashboard: React.FC = () => {
+	return (
+		<S.Container>
+			<S.Title>
+				Dashboard
+			</S.Title>
+		</S.Container>
+	)
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1 ;
+	align-items: center; 
+	justify-content: center;
+	padding: 16px;
+`;
+export const Title = styled.Text`
+	font-size: 40px;
+	color: #7159c1;
+	font-weight: bold;
+`;

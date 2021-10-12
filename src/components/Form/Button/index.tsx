8cb31a18c770708f43c925de64f908f9c90c9543
Carlos Daniel <killer.dk20@gmail.com>
2021-10-12 20:42:03 -0300

    chore: refactor Form Button for Android Devices

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { TouchableOpacityProps, View } from 'react-native';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import * as S from './styles';
 
-interface Props extends TouchableOpacityProps {
+interface Props extends RectButtonProps {
 	title: string
+	onPress: () => void;
 }
 
-export const Button: React.FC<Props> = ({title, ...rest}) => {
+export const Button: React.FC<Props> = ({
+	title,
+	onPress,
+	...rest
+}) => {
 	return (
-		<S.Container {...rest}>
+		<S.Container onPress={onPress} {...rest}>
 			<S.Title>
 				{title}
 			</S.Title>

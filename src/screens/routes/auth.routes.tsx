0a243c7acbfb3611a25c94753477e04ffb670c1b
Carlos Daniel <killer.dk20@gmail.com>
2021-10-23 16:51:38 -0300

    chore: separate Auth Routes

---
+++ src/screens/routes/auth.routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SignIn } from '../SignIn';
+
+const { Navigator, Screen } = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <Navigator
+      headerMode="none"
+    >
+      <Screen
+        name="SignIn"
+        component={SignIn}
+      />
+    </Navigator>
+  );
+}
+
+export default AuthRoutes;
